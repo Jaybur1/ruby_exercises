324215f845ace81df070c7fe7f39872ee2d29b9b
Jay Burbyga <jayburbyga@gmail.com>
2019-02-10 19:35:25 -0500

    pig latin solution added

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,79 @@
-#write your code here
+#handle echo
+def echo(str)
+  str
+end
+
+#handle shout
+def shout(str)
+str.upcase!
+end
+
+#handle repeat
+def repeat(str,num = 2)
+    repeat_str = Array.new
+    num.times {repeat_str << str}
+    repeat_str.join(" ")
+end
+
+#handle start of word
+def start_of_word(str,index)
+    if str.length > 1
+       str.slice(0,index)
+    else
+    alphabet= ("a".."z").to_a
+    alphabet_seq = ("a"..alphabet[index]).to_a
+    alphabet_seq.join
+    end
+end
+
+
+#handle first word
+def first_word(str)
+    str_arr = str.split(" ")
+    str_arr[0]
+end
+
+#handle titleize
+def titleize(str)
+    str_arr = str.split
+    little_words = ["and", "the", "over", "is","has","was"]
+    str_arr[0].capitalize!
+    str_arr.each {|word| word.capitalize! if little_words.index(word) == nil }
+    str_arr.join(" ")
+end
+
+# handle display
+#delete the =begin and =end to see interactive console in your terminal
+=begin
+puts "Welcome to simon says game! \nPlease, write a choice of action"
+puts "echo\nshout\nrepeat\nstart of word\nfirst word\ntitleize"
+action = gets.chomp
+
+case action
+  when "echo"
+    puts "Give me something to echo"
+    echo_str = gets.chomp
+    puts echo(echo_str)
+  when "shout"
+    puts "Give me something to shout"
+    shout_str = gets.chomp
+    puts shout(shout_str)
+  when "repeat"
+    puts "Give me something to repeat"
+    repeat_str = gets.chomp
+    puts "How many times you want me to repeat?"
+    repeat_num = gets.chomp.to_i
+    puts repeat(repeat_str,repeat_num)
+  when "start of word"
+    puts "Give me a word please"
+    strat_of = gets.chomp
+    puts "how many of that latter do you want me to show?"
+    start_of_num = gets.chomp.to_i
+    puts start_of_word(start_of,start_of_num)
+  when "titleize"
+    puts "Give me something to titleize"
+    title = gets.chomp
+    puts titleize(title)
+  else "Sorry mate, I don't know that action."
+end
+=end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,47 @@
-#write your code here
+#handle translate
+def translate(str)
+    str_arr = str.split
+    pig_latin = Array.new
+    str_arr.each do |word|
+#handle word starts whit a vowel
+      if word.start_with?(/[aeiou]/)
+        word << "ay"
+        pig_latin << word
+#handle qu
+     elsif word.include? "qu"
+        cons = word.index(/[aeio]/,0)
+        word = "#{word[cons..-1]}#{word[0..(cons-1)]}ay"
+        pig_latin << word
+#handle capitalized word
+     elsif word.start_with?(/[A-Z]/)
+        word.downcase!
+        cons = word.index(/[aeiou]/,0)
+        word = "#{word[cons..-1]}#{word[0..(cons-1)]}ay"
+        pig_latin << word.capitalize!
+#handle a word that starts with a consonant sound
+     elsif word =~ /[aeiou]/
+        cons = word.index(/[aeiou]/,0)
+        word = "#{word[cons..-1]}#{word[0..(cons-1)]}ay"
+        pig_latin << word
+#handle word without any vowels
+     else
+        word = "#{word}way"
+        pig_latin << word
+      end
+
+    end
+    pig_latin.join(' ')
+end
+
+
+puts translate("squalify Computer aloooo")
+
+
+#remove the comment tags to get interactive console in the terminal
+=begin
+#handle display
+puts "===Pig Latin==="
+puts "Give me some stuff to translate OYK"
+answer = gets.chomp
+puts translate(answer)
+=end
