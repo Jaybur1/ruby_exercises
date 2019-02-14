b93bc727e44a2ad26ca8e9e7468ae56c8762fae2
Jay Burbyga <jayburbyga@gmail.com>
2019-02-13 19:35:57 -0500

    compleated book titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,15 @@
 class Book
-# write your code here
+	attr_accessor  :title
+
+  	def title=(str)
+  	  little_words_arr = ['the','in','over','and', 'on','a', 'an','of']
+		  words_arr = str.split
+		    words_arr[0].capitalize!
+		  words_arr.each do |word|
+			  if !little_words_arr.include? word
+				  word.capitalize!
+			  end
+		  end
+	    @title = words_arr.join(' ')
+	  end
 end
