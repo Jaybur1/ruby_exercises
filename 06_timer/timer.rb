0ef32254842c5c2e1b52ba11c75d2dfa5540ac6d
Jay Burbyga <jayburbyga@gmail.com>
2019-02-13 20:02:11 -0500

    completed timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,21 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+    @seconds = 0
+  end
+  def seconds=(sec)
+    @seconds = sec
+  end
+#handle number to timer transletor
+  def translate(num)
+    num < 10 ? "0#{num}" : "#{num}"
+  end
+#handle timer
+  def time_string
+    sec = @seconds % 60
+    min = @seconds / 60 % 60
+    hour = @seconds / 60 / 60
+    "#{translate(hour)}:#{translate(min)}:#{translate(sec)}"
+  end
 end
+
