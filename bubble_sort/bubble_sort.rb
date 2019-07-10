5887e85e6a5ca4f1cec462c651d5d3f2c1265547
Jay Burbyga <jayburbyga@gmail.com>
2019-07-10 17:18:44 -0400

    bubblesort added

--- bubble_sort/bubble_sort.rb
+++ bubble_sort/bubble_sort.rb
@@ -20,7 +20,31 @@ def bubble_sort(arr)
 end
 
 
+def bubble_sort_by(arr)
+ index = 0
+ err = 0
+ while index < arr.length-1
+   if yield(arr[index],arr[index + 1]) > 0
+     big = arr[index]
+     small = arr[index + 1]
+     arr[index] = small
+     arr[index + 1] = big
+     err += 1
+   end
+   if index == arr.length - 2 && err > 0
+     index = 0
+     err = 0
+   else
+   index += 1
+   end
+ end
+ arr
+end
+
 bubble_sort([4,3,78,2,0,2]) 
+bubble_sort_by(["hi","hello","hey"]) {|left,right|
+left.length - right.length}
 
 
 #[0,2,2,3,4,78]
+#["hi", "hey", "hello"]
